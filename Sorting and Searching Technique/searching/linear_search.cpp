23311383ef9d338805aff38a9192ec0c6a8bb33b
Vishal Bhingarde <bhingardevishal5@gmail.com>
2025-11-28 14:49:15 +0530

    Add files via upload

---
+++ Sorting and Searching Technique/searching/linear_search.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int linearSearch(const vector<int>& arr, int target) {
+    for (int i = 0; i < arr.size(); i++)
+        if (arr[i] == target)
+            return i;
+    return -1;
+}

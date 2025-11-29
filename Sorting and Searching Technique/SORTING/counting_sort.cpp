2223a00cd700a632796e361a9e9036ceb8bddad8
Vishal Bhingarde <bhingardevishal5@gmail.com>
2025-11-29 19:47:25 +0530

    Add files via upload

---
+++ Sorting and Searching Technique/SORTING/counting_sort.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void countingSort(vector<int>& arr) {
+    int maxVal = *max_element(arr.begin(), arr.end());
+    vector<int> count(maxVal + 1, 0);
+    for (int x : arr) count[x]++;
+    int idx = 0;
+    for (int i = 0; i <= maxVal; i++)
+        while (count[i]--) arr[idx++] = i;
+}
